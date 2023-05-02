965c3e593812560d09f31aee4080024c0f8693c0
Adriano Gomes <adrianosan23@gmail.com>
2023-05-02 11:15:24 -0400

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nicole/exemplo-ci',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '96fe01bfd4df0711489771da832fb50c1574cb51',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nicole/exemplo-ci' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '96fe01bfd4df0711489771da832fb50c1574cb51',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
