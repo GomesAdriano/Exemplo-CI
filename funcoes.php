96fe01bfd4df0711489771da832fb50c1574cb51
Adriano Gomes <adrianosan23@gmail.com>
2023-05-02 10:52:21 -0400

    Update funcoes.php

--- funcoes.php
+++ funcoes.php
@@ -4,6 +4,11 @@ function somar($a, $b) {
   return $a + $b;
 }
 
+function multiplicar($a, $b) {
+  return $a * $b;
+}
+
 echo "Soma: " . somar(2, 3) . "\n";
+echo "Multiplicação: " . multiplicar(2, 3) . "\n";
 
 ?>
